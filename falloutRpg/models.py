edb121ed51c4909e9ffd29d1d434ade9885a5aec
bonesinit <72310341+bonesinit@users.noreply.github.com>
2023-10-21 13:56:06 -0400

    Update models.py
    Added weapon and armor models

--- falloutRpg/models.py
+++ falloutRpg/models.py
@@ -33,7 +33,6 @@ class Perk(models.Model):
     def __str__(self):
         return self.name
 
-
 class Character(models.Model):
     # This model contains the data of each character that a user has.
     # A user can have multiple characters.
@@ -136,9 +135,10 @@ class Character(models.Model):
     # ---------------------------------------------------------------------------------------------
     #       CONDITION
     # ---------------------------------------------------------------------------------------------
-    # The character's physical health.
+    # The character's physical health, base armor class, and rads.
 
     hit_points = models.IntegerField(default=0)
+    base_armor = models.IntegerField(default=0)
     rads = models.IntegerField(default=0, validators=[MaxValueValidator(1000), MinValueValidator(0)])
 
     # ---------------------------------------------------------------------------------------------
@@ -157,12 +157,148 @@ class Character(models.Model):
     level = models.IntegerField(default=1)
 
     # ---------------------------------------------------------------------------------------------
-    #       EQUIPMENT
+    #       WEAPON
     # ---------------------------------------------------------------------------------------------
+    # Each character has a list of weapons that is separate from their inventory.
+
+    # ---------------------------------------------------------------------------------------------
+    #       ARMOR
+    # ---------------------------------------------------------------------------------------------
+    # Each character has a list of weapons that is separate from their inventory.
 
     # ---------------------------------------------------------------------------------------------
     #       INVENTORY
     # ---------------------------------------------------------------------------------------------
 
+    def __str__(self):
+        return self.name
+
+class Weapon(models.Model):
+    # A character can have many weapons.
+
+    # ---------------------------------------------------------------------------------------------
+    #       DEFINING CHOICES
+    # ---------------------------------------------------------------------------------------------
+
+    # Range of weapon
+    CLOSE = "CL"
+    SHORT = "SH"
+    MEDIUM = "ME"
+    LONG = "LO"
+
+    RANGE_CHOICES = [
+        ("CL", "Close"),
+        ("SH", "Short"),
+        ("ME", "Medium"),
+        ("LO", "Long"),
+    ]
+
+    # Type of weapon
+    ENERGY = "EN"
+    GUNS = "GU"
+    MELEE = "ME"
+    UNARMED = "UN"
+
+    TYPE_CHOICES = [
+        ("EN", "Energy"),
+        ("GU", "Guns"),
+        ("ME", "Melee"),
+        ("UN", "Unarmed"),
+    ]
+
+    # ---------------------------------------------------------------------------------------------
+
+    # Who this weapon is associated with.
+    owner = models.ForeignKey(Character, on_delete=models.CASCADE())
+
+    # Name of weapon.
+    name = models.CharField(max_length=64)
+
+    # Type of weapon
+    type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=ENERGY,
+    )
+
+    # Range of weapon (close is melee range)
+    range = models.CharField(
+        max_length=2,
+        choices=RANGE_CHOICES,
+        default=CLOSE,
+    )
+
+    # Damage per shot
+    damage = models.IntegerField(default=0)
+
+    # Burst fire capable?
+    is_burst = models.BooleanField(default=False)
+
+    # Action point costs for single shot, targeted shot, or burst
+    ap_cost_single = models.IntegerField(default=0)
+    ap_cost_targeted = models.IntegerField(default=0)
+    ap_cost_burst = models.IntegerField(default=0)
+
+    # Ammo
+    ammo_count = models.IntegerField(default=0)
+    ammo_type = models.CharField(max_length=24)
+    ammo_per_shot = models.IntegerField(default=1)
+    ammo_per_burst = models.IntegerField(default=3)
+
+    # Special abilities
+    special_abilities = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Armor(models.Model):
+    # A character can have many pieces of armor.
+
+    # ---------------------------------------------------------------------------------------------
+    #       DEFINING CHOICES
+    # ---------------------------------------------------------------------------------------------
+
+    # Type of weapon
+    UNARMORED = "UN"
+    LIGHT = "LI"
+    MEDIUM = "ME"
+    HEAVY = "HE"
+
+    TYPE_CHOICES = [
+        ("UN", "Unarmored"),
+        ("LI", "Light"),
+        ("ME", "Medium"),
+        ("HE", "Heavy"),
+    ]
+
+    # ---------------------------------------------------------------------------------------------
+
+    # Who this armor is associated with
+    owner = models.ForeignKey(Character, on_delete=models.CASCADE())
+
+    # Name of armor
+    name = models.CharField(max_length=64)
+
+    # Type of armor. Med reduces agility by 1, heavy reduces agility by 2
+    type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=UNARMORED,
+    )
+
+    # Effects on stats, special abilities
+    effects = models.CharField(max_length=128)
+
+    # Armor class
+    armor_class = models.IntegerField(default=0)
+
+    # Damage reduction
+    damage_reduction = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Inventory(models.Model):
+    owner = models.ForeignKey(Character, on_delete=models.CASCADE())
 
 
