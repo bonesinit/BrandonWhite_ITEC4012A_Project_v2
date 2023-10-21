1579314e7a9cd77855dd70cfc65a7d272a4ad7d0
bonesinit <72310341+bonesinit@users.noreply.github.com>
2023-10-21 15:41:05 -0400

    Update urls.py, admin.py
    Adding admin functions

--- falloutRpg/admin.py
+++ falloutRpg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Character, Inventory, Inventory_Item, Perk, Weapon, Armor
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Inventory)
+admin.site.register(Inventory_Item)
+admin.site.register(Perk)
+admin.site.register(Weapon)
+admin.site.register(Armor)
